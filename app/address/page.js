4351ee12394942df87a63a0f060d2eac1aa5c3f7
droese <droese@autarsys.com>
2025-04-12 14:33:06 +0200

    Conflict resolved2

--- app/address/page.js
+++ app/address/page.js
@@ -1,11 +1,11 @@
-<<<<<<< HEAD
+// <<<<<<< HEAD
+// // // app/address/page.js
+// // import AddressComponent from '@/components/AddressComponent';
+// =======
 // // app/address/page.js
-// import AddressComponent from '@/components/AddressComponent';
-=======
-// app/address/page.js
-import AddressComponent from '@/components/AddressComponent'
->>>>>>> d76a44a9390ed68f6e28d3b5cde1211b36d57373
+// import AddressComponent from '@/components/AddressComponent'
+// >>>>>>> d76a44a9390ed68f6e28d3b5cde1211b36d57373
 
-// export default function AddressPage({ orderSummary, user }) {
-//   return <AddressComponent orderSummary={orderSummary} user={user} />
-// }
+// // export default function AddressPage({ orderSummary, user }) {
+// //   return <AddressComponent orderSummary={orderSummary} user={user} />
+// // }
